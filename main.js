315f926b4a9fe6df20f8f56d11684fd64e094dd0
Saiprava1234 <86531553+Saiprava1234@users.noreply.github.com>
2021-09-09 14:46:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Ranbir";
+console.log(first_name);
+
+var last_name = "Kapoor";
+console.log(first_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
